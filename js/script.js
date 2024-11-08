32d6c4c8317c48eab29e359d3596d9c0c76db05b
ArthurZ7 <arthuriborges100@gmail.com>
2024-11-08 17:33:36 -0300

    html e css desktop

--- js/script.js
+++ js/script.js
@@ -1,26 +1,55 @@
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+let imagemAtual = document.querySelectorAll(".img-carrosel");
+const setaDireita = document.querySelector(".seta-direita");
+const setaEsquerda = document.querySelector(".seta-esquerda");
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const imagens = [
+  "images/carrosel/carrosel1.png",
+  "images/carrosel/carrosel2.jpg",
+  "images/carrosel/carrosel3.jpeg",
+];
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+console.log(imagemAtual);
+console.log(setaDireita);
+
+imagemAtual.forEach((img) => {
+  setaDireita.addEventListener("click", () => {
+    for (let i = 0; i < imagens.length; i++) {
+      if (img.src.includes(imagens[i])) {
+        if (i === imagens.length - 1) {
+          img.src = imagens[0];
+        } else {
+          img.src = imagens[i + 1];
+        }
+        break;
+      }
+    }
+  });
+  console.log(imagemAtual);
+  setaEsquerda.addEventListener("click", () => {
+    for (let i = 0; i < imagens.length; i++) {
+      if (img.src.includes(imagens[i])) {
+        if (i === 0) {
+          img.src = imagens[imagens.length - 1];
+        } else {
+          img.src = imagens[i - 1];
+        }
+        break;
+      }
+    }
+  });
+});
+setInterval(() => {
+  imagemAtual.forEach((img) => {
+    for (let i = 0; i < imagens.length; i++) {
+      if (img.src.includes(imagens[i])) {
+        if (i === imagens.length - 1) {
+          img.src = imagens[0];
+        } else {
+          img.src = imagens[i + 1];
+        }
+        break;
+      }
+    }
+  });
+}, 5000);
